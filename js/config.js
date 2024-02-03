0dc545df534bbcbb7b2db777ae7069c1ede6595c
jonathan-Cohen <jonathan.cohen@epitech.eu>
2024-02-03 14:50:40 +0100

    add cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_8f6D0LI5g', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'crgrg1v40odp5tq2v4jbia2lt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-south-1' // e.g. us-east-2
+        userPoolId: 'eu-central-1_3rBpdMX1u', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1ujjfusa86gs31dbnpikbaf4lj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
